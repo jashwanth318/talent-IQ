4f5bade3ea6d19ba40f7244dc58a5b340a7ab083
Marati Jashwanth Kumar <jashwanthkumar318@example.com>
2025-11-13 16:48:21 +0530

    auth & stream sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "talent-iq" });
 
@@ -22,7 +23,11 @@ const syncUser = inngest.createFunction(
 
     await User.create(newUser);
 
-    //todo: do sth else
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage,
+    });
   }
 );
 
@@ -35,8 +40,8 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
 
-    //todo: do sth else
+    await deleteStreamUser(id.toString());
   }
 );
 
-export const functions = [syncUser,deleteUserFromDB];
+export const functions = [syncUser, deleteUserFromDB];
